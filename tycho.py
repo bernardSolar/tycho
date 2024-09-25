84ac03cf58d69a2968f5265c24e627a5d85fb403
bernard <bernard.mccarty@gmail.com>
2024-09-25 08:19:43 +0100

    Update tycho.py
    defaults to CLIPS_ALL_VIDEOS.db

--- tycho.py
+++ tycho.py
@@ -43,7 +43,7 @@ auth = dash_auth.BasicAuth(
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
-db_path = 'cd_gpt3_5_definitions.db'
+db_path = 'CLIPS_ALL_VIDEOS.db'
 conn = sqlite3.connect(db_path)
 query = "SELECT * FROM descriptions"  # Adjust if your table name or structure is different
 df = pd.read_sql_query(query, conn)
